f8b25d6085d248f355000544f70bff21e1b5090d
MrSz-84 <pixelmotionhd@gmail.com>
2024-06-27 23:13:45 +0200

    Added function for validation of error message elements in return(requests_url()
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- utils/requests.py
+++ utils/requests.py
@@ -1,3 +1,5 @@
+import config.config as config
+import json
 
 
 def return_request_url(req_type, blog_url=None, blog_id=None, post_id=None, auth=None, phrase=None,
@@ -14,21 +16,34 @@ def return_request_url(req_type, blog_url=None, blog_id=None, post_id=None, auth
         'by_path': f'{base_req_body}{blog_id}/posts/bypath?path={post_path}&key={auth}',
         'comments': f'{base_req_body}{blog_id}/posts/{post_id}/comments?key={auth}',
     }
-    if req_type not in req_types.keys() or 'None' in req_types[req_type]:
+    none_val_in_req = validate_error_message(req_type, req_types)
+    if req_type not in req_types.keys() or 'None' in req_types.get(req_type):
         raise ValueError(f'Variable req_type not included in possible requests: '
                          f'{req_type not in req_types}, \nor variables needed for request creation '
-                         f'are missing: {'None' in req_types[req_type]}')
+                         f'are missing: {none_val_in_req}')
     else:
-        return req_types[req_type]
+        return req_types.get(req_type)
 
 
+def validate_error_message(request, requests):
+    if request not in requests.keys():
+        return False
+    else:
+        try:
+            none_present = 'None' in requests.get(request)
+        except TypeError:
+            return False
+    return none_present
 
-auth_ = "AIzaSyAJ-vz3ugbpJ4fWwNOnpqTBBNG5cVL5bxY"
+
+with open(config.KEY, encoding='utf-8') as file:
+    auth_ = json.load(file)['Authorization']
 API_BASE_REQ = 'https://www.googleapis.com/blogger/v3/blogs/'
 BLOG_ID = '3417787614555634377'
 BLOG_URL = 'https://smakolykibereniki.blogspot.com/'
 
 
+# return_request_url(req_type='', base_req_body=API_BASE_REQ)
 aaa = return_request_url(req_type='by_url', auth=auth_, blog_url=BLOG_URL, base_req_body=API_BASE_REQ)
 print(aaa)
 
