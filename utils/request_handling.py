64f94300018e297097013701fc121180b5e17547
MrSz-84 <pixelmotionhd@gmail.com>
2024-06-30 19:59:37 +0200

    Added checking for blogger, url validity and connection validity. Creation of test for them remains. Functions toi test: input_func, input_blog_address, validate_blog_name,
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>
    #  check_response_code, is_blogger, get_blog_id

--- utils/request_handling.py
+++ utils/request_handling.py
@@ -2,6 +2,92 @@ import re
 import json
 import requests
 import config.config as config
+import utils.various as various
+
+
+def input_func():
+    return input('Type in blog`s name: ')
+
+
+def input_blog_address():
+    print('What blog hosted on Blogger would you like to see? Omit .blogspot.com and http parts.')
+    blog_name = input_func()
+    ok_name = False
+    ok_conn = False
+    while not ok_name and not ok_conn:
+        ok_name = validate_blog_name(blog_name)
+        if not ok_name:
+            print('Inputted value contains more than blog`s name.')
+            blog_name = input_func()
+            continue
+        ok_conn = check_response_code(blog_name)
+        if not ok_conn:
+            print('Requested blog seems to not respond, try one more time:')
+            blog_name = input_func()
+            ok_name = False
+    blog_url = f'https://{blog_name}.blogspot.com/'
+    return blog_url
+
+
+def validate_blog_name(name):
+    user_input = name.lower().strip()
+    hits = re.compile(r'^https\D{3}|^http\D{3}|\Dblogspot|\Dblogger|\Dcom|\Dcom$|/$')
+    if not bool(re.search(hits, user_input)):
+        print('Name Valid')
+        return True
+    else:
+        print('Name Invalid')
+        return False
+
+
+def check_response_code(to_check):
+    url = f'https://{to_check}.blogspot.com/'
+    try:
+        with requests.get(url, stream=True) as r:
+            if r.status_code == 200:
+                print('Connection Valid')
+                return True
+            else:
+                print('Connection Invalid')
+                return False
+    except requests.ConnectionError:
+        print('Connection Error')
+        return False
+
+
+def is_blogger(url):
+    with requests.get(url, stream=True) as r:
+        link = re.compile(r'.blogger.com/')
+        generator = re.compile(r"<meta content='blogger' name='generator'/>")
+        feed = re.compile(r'/feeds/posts/default')
+        blogger_in_link = False
+        blogger_as_generator = False
+        feed_in_link = False
+        for chunk in r.iter_content(chunk_size=1024, decode_unicode=True):
+            if not isinstance(link, re.Match):
+                blogger_in_link = bool(re.search(link, chunk))
+            if not isinstance(generator, re.Match):
+                blogger_as_generator = bool(re.search(generator, chunk))
+            if not isinstance(feed, re.Match):
+                feed_in_link = bool(re.search(feed, chunk))
+            if all([blogger_in_link, blogger_as_generator, feed_in_link]):
+                return True
+
+
+def get_blog_id(url):
+    blog_pattern = re.compile(r'blog-([0-9]*)<', re.I)
+    blogger = re.compile(r'tag:blogger.com')
+    # user_pattern = re.compile(r'profile/([0-9]*)<', re.I)
+    blog_id = ''
+    url = f'{url}/feeds/posts/default'
+    with requests.get(url, stream=True) as r:
+        for chunk in r.iter_content(chunk_size=1600, decode_unicode=True):
+            if not isinstance(blogger, re.Match):
+                blogger = re.search(blogger, chunk)
+            if not isinstance(blog_id, re.Match):
+                blog_id = re.search(blog_pattern, chunk)
+            if blog_id is not None:
+                return blog_id.group(1)
 
 
 def return_request_url(req_type, blog_url=None, blog_id=None, post_id=None, auth=None, phrase=None,
@@ -36,29 +122,3 @@ def validate_error_message(request, requests_pool):
         except TypeError:
             return False
     return none_present
-
-
-def get_blog_id(url):
-    blog_pattern = re.compile(r'blog-([0-9]*)<', re.I)
-    blogger = re.compile(r'tag:blogger.com')
-    # user_pattern = re.compile(r'profile/([0-9]*)<', re.I)
-    blog_id = ''
-    url = f'https://{url}/feeds/posts/default'
-    with requests.get(url, stream=True) as r:
-        for chunk in r.iter_content(chunk_size=1600, decode_unicode=True):
-            if not isinstance(blogger, re.Match):
-                blogger = re.search(blogger, chunk)
-                print(blogger)
-                exit()
-            if not isinstance(blog_id, re.Match):
-                blog_id = re.search(blog_pattern, chunk)
-            if blog_id is not None:
-                return blog_id.group(1)
-
-
-
-
-
-
-def is_blogger(url):
-    ...
