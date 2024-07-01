669b53a5d3c7dcb47e9c01960415191feb1bf213
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-01 19:35:16 +0200

    Changed constant name.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- config/config.py
+++ config/config.py
@@ -1,5 +1,4 @@
 KEY = './api_auth/key.json'
 API_BASE_REQ = 'https://www.googleapis.com/blogger/v3/blogs/'
 BLOG_ID = '3417787614555634377'
-BLOG_URL = 'https://smakolykibereniki.blogspot.com/'
-
+BASE_BLOG_URL = 'smakolykibereniki.blogspot.com'
