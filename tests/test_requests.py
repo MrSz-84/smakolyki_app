5dd3f78905464d854b1854e83ab0170c20958ebd
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-01 23:04:21 +0200

    Provided tests for input_func(), and most of tests for input_blog_address. For the partial tests only assertion of stdout remaining.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,7 +1,8 @@
 import pytest
 import json
+import sys
 from config import config as cnf
-from utils import requests as req
+from utils import request_handling as req
 
 
 with open(cnf.KEY, encoding='utf-8') as file:
@@ -54,15 +55,15 @@ def test_req_types_return():
     assert req.return_request_url(req_type=request, blog_id=id_, auth=api_key,
                                   base_req_body=body) == by_id_result
     request = 'by_url'
-    url = cnf.BLOG_URL
+    url = cnf.BASE_BLOG_URL
     body = cnf.API_BASE_REQ
-    by_url_result = f'{cnf.API_BASE_REQ}byurl?url={cnf.BLOG_URL}?key={api_key}'
+    by_url_result = f'{cnf.API_BASE_REQ}byurl?url={cnf.BASE_BLOG_URL}?key={api_key}'
     assert req.return_request_url(req_type=request, auth=api_key, base_req_body=body,
                                   blog_url=url) == by_url_result
     request = 'by_url'
     url = 'foobazbar'
     body = cnf.API_BASE_REQ
-    by_url_result = f'{cnf.API_BASE_REQ}byurl?url={cnf.BLOG_URL}?key={api_key}'
+    by_url_result = f'{cnf.API_BASE_REQ}byurl?url={cnf.BASE_BLOG_URL}?key={api_key}'
     assert req.return_request_url(req_type=request, auth=api_key, base_req_body=body,
                                   blog_url=url) != by_url_result
 
@@ -81,3 +82,68 @@ def test_req_types_return_exception_raising():
         by_id_result = f'https://www.googleapis.com/blogger/v3/blogs/{cnf.BLOG_ID}?key={api_key}'
         assert req.return_request_url(req_type=request, auth=api_key,
                                       base_req_body=body)
+
+
+def test_check_input_func(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: 'this is test text')
+    assert req.input_func() == 'this is test text'
+    monkeypatch.setattr('builtins.input', lambda _: 'some blog name here')
+    assert req.input_func() == 'some blog name here'
+    assert req.input_func() != 'foobarbaz'
+
+
+def test_raise_error_input_func(monkeypatch):
+    monkeypatch.setattr(sys.stdin, 'readline', lambda: 'foo')
+    with pytest.raises(AssertionError):
+        assert req.input_func() == 'foobar'
+    monkeypatch.setattr(req, 'input_func', lambda: '')
+    with pytest.raises(AssertionError):
+        assert req.input_func() == '1'
+
+
+def test_input_blog_address_goes_towards_validation(monkeypatch):
+    valid = 'learningpythonway'
+    invalid = 'http://foobar.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid, valid])
+
+    def mock_input_func():
+        return next(user_inputs)
+
+    def mock_validate_blog_name(name):
+        return name == valid
+
+    def mock_check_response_code(name):
+        return name == valid
+
+    monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+    monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+    monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+
+    assert req.input_blog_address() == f'https://{valid}.blogspot.com/'
+
+
+def test_input_blog_address_raises_runtime(monkeypatch):
+    valid = 'learningpythonway'
+    invalid = 'http://foobar.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid])
+
+    def mock_input_func():
+        return next(user_inputs)
+
+    def mock_validate_blog_name(name):
+        return name == valid
+
+    def mock_check_response_code(name):
+        return name == valid
+
+    monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+    monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+    monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+
+    with pytest.raises(StopIteration):
+        assert req.input_blog_address()
+
+# TODO make assertion to what the function prints to the stdout,
+#  what would simulate the False response from validating functions capsys.stdouterr()
+
+
