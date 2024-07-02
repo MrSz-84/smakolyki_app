b54adc165a1f1de2895c98fb8e5e66b42d6fe830
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-02 22:25:15 +0200

    Added assertion test for displayed text in input_blog_address() function.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -22,29 +22,29 @@ test_requests = {
 def test_validate_error_message_key_present():
     requests = test_requests.copy()
     request = 'by_id'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
     request = 'by_url'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
     request = 'posts'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
     request = 'search'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
     request = 'by_path'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
     request = 'comments'
-    assert req.validate_error_message(request, requests) is True
+    assert req.validate_error_message(request, requests) is True, 'should be True but was False'
 
 
 def test_validate_error_message_key_absent():
     requests = test_requests.copy()
     request = 'by_by'
-    assert req.validate_error_message(request, requests) is False
+    assert req.validate_error_message(request, requests) is False, 'should be False but was True'
     request = 'foo'
-    assert req.validate_error_message(request, requests) is False
+    assert req.validate_error_message(request, requests) is False, 'should be False but was True'
     request = 'bar'
-    assert req.validate_error_message(request, requests) is False
+    assert req.validate_error_message(request, requests) is False, 'should be False but was True'
     request = 'i hate python`s path management'
-    assert req.validate_error_message(request, requests) is False
+    assert req.validate_error_message(request, requests) is False, 'should be False but was True'
 
 
 def test_req_types_return():
@@ -85,6 +85,11 @@ def test_req_types_return_exception_raising():
 
 
 def test_check_input_func(monkeypatch):
+    """
+    Test function that simulates inputs given by the user.
+    :param monkeypatch: Pytest's built-in fixture
+    :return: None
+    """
     monkeypatch.setattr('builtins.input', lambda _: 'this is test text')
     assert req.input_func() == 'this is test text'
     monkeypatch.setattr('builtins.input', lambda _: 'some blog name here')
@@ -93,6 +98,12 @@ def test_check_input_func(monkeypatch):
 
 
 def test_raise_error_input_func(monkeypatch):
+    """
+    Test function that simulates inputs given by the user. Tests exceptions raising.
+    :param monkeypatch: Pytest's built-in fixture
+    :return: None
+    """
+
     monkeypatch.setattr(sys.stdin, 'readline', lambda: 'foo')
     with pytest.raises(AssertionError):
         assert req.input_func() == 'foobar'
@@ -102,6 +113,11 @@ def test_raise_error_input_func(monkeypatch):
 
 
 def test_input_blog_address_goes_towards_validation(monkeypatch):
+    """
+    Test for simulating user inputs ending with positive input and assertion
+    :param monkeypatch: Pytest's built-in fixture
+    :return: None
+    """
     valid = 'learningpythonway'
     invalid = 'http://foobar.blogspot.com'
     user_inputs = iter([invalid, invalid, invalid, valid])
@@ -123,6 +139,11 @@ def test_input_blog_address_goes_towards_validation(monkeypatch):
 
 
 def test_input_blog_address_raises_runtime(monkeypatch):
+    """
+    Test for simulating user inputs, ending with no valid input and exception raising
+    :param monkeypatch: Pytest's built-in fixture
+    :return: None
+    """
     valid = 'learningpythonway'
     invalid = 'http://foobar.blogspot.com'
     user_inputs = iter([invalid, invalid, invalid])
@@ -143,7 +164,67 @@ def test_input_blog_address_raises_runtime(monkeypatch):
     with pytest.raises(StopIteration):
         assert req.input_blog_address()
 
-# TODO make assertion to what the function prints to the stdout,
-#  what would simulate the False response from validating functions capsys.stdouterr()
+
+def test_input_blog_address_output_name_validation(monkeypatch, capsys):
+    """
+    Test for checking if proper messages are presented to the user while inputting invalid values.
+    It simulates inputting unaccepted blob's name.
+    Ends with raising StopIteration.
+    :param monkeypatch: Pytest's built-in fixture
+    :param capsys: Pytest's built-in fixture
+    :return: None
+    """
+    valid = 'learningpythonway'
+    invalid = 'http://foobar.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid])
+
+    def mock_input_func():
+        return next(user_inputs)
+
+    def mock_validate_blog_name(name):
+        return name == valid
+
+    def mock_check_response_code():
+        return True
+
+    monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+    monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+    monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+
+    with pytest.raises(StopIteration):
+        assert req.input_blog_address()
+    captured = capsys.readouterr()
+    assert 'Inputted value contains more than blog`s name.' in captured.out
+    assert 'Requested blog seems to not respond, try one more time.' not in captured.out
 
 
+def test_input_blog_address_output_response_validation(monkeypatch, capsys):
+    """
+    Test designed to check for validity of messages presented to the user, when response code
+    is else than 200.
+    :param monkeypatch: Pytest's built-in fixture
+    :param capsys: Pytest's built-in fixture
+    :return: None
+    """
+    valid = 'learningpythonway'
+    invalid = 'http://foobarbazbazsuperduper.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid])
+
+    def mock_input_func():
+        return next(user_inputs)
+
+    def mock_validate_blog_name(name):
+        return True
+
+    def mock_check_response_code(name):
+        return name == valid
+
+    monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+    monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+    monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+
+    with pytest.raises(StopIteration):
+        assert req.input_blog_address()
+    captured = capsys.readouterr()
+    assert 'Inputted value contains more than blog`s name.' not in captured.out
+    assert 'Requested blog seems to not respond, try one more time.' in captured.out
