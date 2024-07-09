10f5db44d45a52cad65daefb64abd4e97f857931
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-09 23:40:54 +0200

    Refactoring for the usage of asyncio and aiohttp libraries.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import json
 import requests
+import aiohttp
+import asyncio
 import config.config as config
 import utils.request_handling as req
 import time
@@ -11,27 +13,42 @@ def main():
     with open(config.KEY, encoding='utf-8') as file:
         api_key = json.load(file)['Authorization']
 
-    # url = req.return_request_url('by_id', blog_id=config.BLOG_ID, auth=api_key,
-    #                              base_req_body=config.API_BASE_REQ)
-
-    # blog, user = req.get_blog_and_user_id(config.BLOG_URL)
-
-    address = req.input_blog_address()
-    is_blogger = req.is_blogger(address)
-    if is_blogger:
-        blog_id = req.get_blog_id(address)
-        url = req.return_request_url('by_id', blog_id=blog_id, auth=api_key,
-                                     base_req_body=config.API_BASE_REQ)
-        r = requests.get(url)
+    blog_id = req.input_blog_address()
+    # blog_id = req.get_blog_id(address)
+    url = req.return_request_url('by_id', blog_id=blog_id, auth=api_key,
+                                 base_req_body=config.API_BASE_REQ)
+    with requests.Session() as s:
+        r = s.get(url)
         print(r.text)
     end = time.time()
     print(f'Elapsed: {end - start:.2f}')
     # blog = req.get_blog_id('smakolykibereniki.blogspot.com')
     # print(blog)
 
+
+async def create_session():
+    return aiohttp.ClientSession()
+
+
+async def main2():
+    start = time.time()
+    with open(config.KEY, encoding='utf-8') as file:
+        api_key = json.load(file)['Authorization']
+
+    async with await req.ConnectionPool.get_session() as session:
+        blog_id = await req.input_blog_address(session)
+
+
+
+    end = time.time()
+    print(f'Elapsed: {end - start:.2f}')
+
+
 # TODO create tests for new functions: input_func, input_blog_address, validate_blog_name,
 #  check_response_code, is_blogger, get_blog_id
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main2())
+
+
