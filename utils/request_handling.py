2cbedabd612fee56bc0329a860818cc3a15b8581
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-10 23:58:32 +0200

    Refactoring fot results gathering and returning using asyncio. Work in progress on catching exceptions.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- utils/request_handling.py
+++ utils/request_handling.py
@@ -28,14 +28,19 @@ def input_func():
 async def input_blog_address(s):
     print('What blog hosted on Blogger would you like to see? Omit .blogspot.com and http parts.')
     # blog_name = input_func()
-    blog_name = 'googlevideo'
+    blog_name = 'https://googlevideo87263e987?.gog.pl?)__+)L!@*&('
+    blog_url = f'https://{blog_name}.blogspot.com/'
     ok_name = False
     ok_conn = False
     ok_blogger = False
     while not ok_name and not ok_conn and not ok_blogger:
-        ok_name = validate_blog_name(blog_name)
-        ok_conn = await check_response_code(blog_name, s)
-        ok_blogger = await is_blogger(f'https://{blog_name}.blogspot.com/', s)
+        async with asyncio.TaskGroup() as tg:
+            ok_name = tg.create_task(validate_blog_name(blog_name))
+            ok_conn = tg.create_task(check_response_code(blog_name, s))
+            ok_blogger = tg.create_task(is_blogger(blog_url, s))
+            blog_id = tg.create_task(get_blog_id(blog_url, s))
+            ok_name, ok_conn, ok_blogger, blog_id = await asyncio.gather(
+                ok_name, ok_conn, ok_blogger, blog_id)
         if not ok_name:
             print('Inputted value contains more than blog`s name.')
             blog_name = input_func()
@@ -47,13 +52,15 @@ async def input_blog_address(s):
         if not ok_blogger:
             print('Requested blog seems not to be on blogger platform, try one more time.')
             blog_name = input_func()
-            ok_blogger = False
-    blog_url = f'https://{blog_name}.blogspot.com/'
-    blog_id = await get_blog_id(blog_url, s)
+            ok_name, ok_conn = False
+        if not blog_id:
+            print('Requested blog seems not to have an ID, try one more time.')
+            blog_name = input_func()
+            ok_name, ok_conn, ok_blogger = False
     return blog_id
 
 
-def validate_blog_name(name):
+async def validate_blog_name(name):
     user_input = name.lower().strip()
     hits = re.compile(r'^https\D{3}|^http\D{3}|\Dblogspot|\Dblogger|\Dcom|\Dcom$|/$')
     if not bool(re.search(hits, user_input)):
@@ -74,7 +81,7 @@ async def check_response_code(to_check, session):
             else:
                 print('Connection Invalid')
                 return False
-    except (requests.ConnectionError, requests.exceptions.InvalidURL):
+    except (aiohttp.client_exceptions.InvalidURL, aiohttp.client_exceptions.ClientConnectorError):
         print('Connection Error')
         return False
 
@@ -109,14 +116,18 @@ async def get_blog_id(url, session):
     # user_pattern = re.compile(r'profile/([0-9]*)<', re.I)
     blog_id = ''
     url = f'{url}feeds/posts/default'
-    async with session.get(url) as r:
-        r.encoding = 'UTF-8'
-        async for chunk in r.content.iter_chunked(1600):
-            print(chunk)
-            if not isinstance(blog_id, re.Match):
-                blog_id = re.search(blog_pattern, chunk)
-            if blog_id is not None:
-                return blog_id.group(1)
+    try:
+        async with session.get(url) as r:
+            r.encoding = 'utf-8'
+            async for chunk in r.content.iter_chunked(1600):
+                # print(chunk)
+                if not isinstance(blog_id, re.Match):
+                    blog_id = re.search(blog_pattern, chunk)
+                if blog_id is not None:
+                    return blog_id.group(1).decode(encoding='UTF-8')
+            return False
+    except (aiohttp.client_exceptions.InvalidURL, aiohttp.client_exceptions.ClientConnectorError):
+        return False
 
 
 def return_request_url(req_type, blog_url=None, blog_id=None, post_id=None, auth=None, phrase=None,
