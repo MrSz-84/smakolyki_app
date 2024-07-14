11c8d89ef94aa3074600925958f128fdc274a581
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-14 23:15:59 +0200

    Refactored tests for test_response_code.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,13 +1,12 @@
 import pytest
 import json
 import sys
-import requests_mock
 import requests
-import asyncio
 import aiohttp
+import asyncio
 from aiohttp import web
+from pytest_mock import mocker
 
-import utils.request_handling
 from config import config as cnf
 from utils import request_handling as req
 
@@ -26,7 +25,7 @@ test_requests = {
 }
 
 
-def test_validate_error_message_key_present():
+def test_validate_error_message_key_present() -> None:
     request_type = test_requests.copy()
     request = 'by_id'
     assert (
@@ -54,7 +53,7 @@ def test_validate_error_message_key_present():
     ), 'should be True but was False'
 
 
-def test_validate_error_message_key_absent():
+def test_validate_error_message_key_absent() -> None:
     request_type = test_requests.copy()
     request = 'by_by'
     assert (
@@ -74,7 +73,7 @@ def test_validate_error_message_key_absent():
     ), 'should be False but was True'
 
 
-def test_req_types_return():
+def test_req_types_return() -> None:
     request = 'by_id'
     id_ = cnf.BLOG_ID
     body = cnf.API_BASE_REQ
@@ -109,7 +108,7 @@ def test_req_types_return():
     )
 
 
-def test_req_types_return_exception_raising():
+def test_req_types_return_exception_raising() -> None:
     with pytest.raises(ValueError):
         request = 'foobar'
         id_ = cnf.BLOG_ID
@@ -126,7 +125,7 @@ def test_req_types_return_exception_raising():
         )
 
 
-def test_check_input_func(monkeypatch):
+def test_check_input_func(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Test function that simulates inputs given by the user.
     :param monkeypatch: Pytest's built-in fixture
@@ -137,9 +136,11 @@ def test_check_input_func(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: 'some blog name here')
     assert req.input_func() == 'some blog name here'
     assert req.input_func() != 'foobarbaz'
+    monkeypatch.setattr('builtins.input', lambda _: '')
+    assert req.input_func() == cnf.BASE_BLOG_NAME
 
 
-def test_raise_error_input_func(monkeypatch):
+def test_raise_error_input_func(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Test function that simulates inputs given by the user. Tests exceptions raising.
     :param monkeypatch: Pytest's built-in fixture
@@ -155,7 +156,7 @@ def test_raise_error_input_func(monkeypatch):
 
 
 @pytest.mark.asyncio
-async def test_input_blog_address_goes_towards_validation(monkeypatch):
+async def test_input_blog_address_goes_towards_validation(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Test for simulating user inputs ending with positive input and assertion
     :param monkeypatch: Pytest's built-in fixture
@@ -167,19 +168,19 @@ async def test_input_blog_address_goes_towards_validation(monkeypatch):
     valid_url = f'https://{valid}.blogspot.com/'
     id = '12345'
 
-    def mock_input_func():
+    def mock_input_func() -> str:
         return next(user_inputs)
 
-    async def mock_validate_blog_name(name):
+    async def mock_validate_blog_name(name: str) -> bool:
         return name == valid
 
-    async def mock_check_response_code(name, session):
+    async def mock_check_response_code(name: str, session: aiohttp.ClientSession()) -> bool:
         return name == valid
 
-    async def mock_is_blogger(url, session):
+    async def mock_is_blogger(url: str, session: aiohttp.ClientSession()) -> bool:
         return url == valid_url
 
-    async def mock_get_blog_id(url, session):
+    async def mock_get_blog_id(url: str, session: aiohttp.ClientSession()) -> str | bool:
         if url == valid_url:
             return id
         return False
@@ -195,7 +196,7 @@ async def test_input_blog_address_goes_towards_validation(monkeypatch):
 
 
 @pytest.mark.asyncio
-async def test_input_blog_address_raises_runtime(monkeypatch):
+async def test_input_blog_address_raises_runtime(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Test for simulating user inputs, ending with no valid input and exception raising
     :param monkeypatch: Pytest's built-in fixture
@@ -207,26 +208,22 @@ async def test_input_blog_address_raises_runtime(monkeypatch):
     valid_url = f'https://{valid}.blogspot.com/'
     id = '12345'
 
-    def mock_input_func():
+    def mock_input_func() -> str:
         return next(user_inputs)
 
-    async def mock_validate_blog_name(name):
+    async def mock_validate_blog_name(name: str) -> bool:
         return name == valid
 
-    async def mock_check_response_code(name, session):
+    async def mock_check_response_code(name: str, session: aiohttp.ClientSession()) -> bool:
         return name == valid
 
-    async def mock_is_blogger(url, session):
-        if url == valid_url:
-            return True
+    async def mock_is_blogger(url: str, session: aiohttp.ClientSession()) -> bool:
+        return url == valid_url
+
+    async def mock_get_blog_id(url: str, session: aiohttp.ClientSession()) -> str | bool:
         return False
 
-    async def mock_get_blog_id(url, session):
-        if url == valid_url:
-            return id
-        return False
-
-    async with aiohttp.ClientSession() as session:
+    async with (aiohttp.ClientSession() as session):
         monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
         monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
         monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
@@ -238,7 +235,8 @@ async def test_input_blog_address_raises_runtime(monkeypatch):
 
 
 @pytest.mark.asyncio
-async def test_input_blog_address_output_name_validation(monkeypatch, capsys):
+async def test_input_blog_address_output_name_validation(monkeypatch: pytest.MonkeyPatch,
+                                                         capsys: pytest.fixture) -> None:
     """
     Test for checking if proper messages are presented to the user while inputting invalid values.
     It simulates inputting unaccepted blob's name.
@@ -253,19 +251,19 @@ async def test_input_blog_address_output_name_validation(monkeypatch, capsys):
     valid_url = f'https://{valid}.blogspot.com/'
     id = '12345'
 
-    def mock_input_func():
+    def mock_input_func() -> str:
         return next(user_inputs)
 
-    async def mock_validate_blog_name(name):
+    async def mock_validate_blog_name(name: str) -> bool:
         return name == valid
 
-    async def mock_check_response_code(name, session):
+    async def mock_check_response_code(name: str, session:aiohttp.ClientSession) -> bool:
         return True
 
-    async def mock_is_blogger(url, session):
+    async def mock_is_blogger(url: str, session:aiohttp.ClientSession) -> bool:
         return True
 
-    async def mock_get_blog_id(url, session):
+    async def mock_get_blog_id(url: str, session:aiohttp.ClientSession) -> str | bool:
         return id
 
     async with aiohttp.ClientSession() as session:
@@ -282,8 +280,10 @@ async def test_input_blog_address_output_name_validation(monkeypatch, capsys):
         assert 'Requested blog seems to not respond, try one more time.' not in captured.out
 
 
+
 @pytest.mark.asyncio
-async def test_input_blog_address_output_response_validation(monkeypatch, capsys):
+async def test_input_blog_address_output_response_validation(monkeypatch: pytest.MonkeyPatch,
+                                                             capsys: pytest.fixture) -> None:
     """
     Test designed to check for validity of messages presented to the user, when response code
     is else than 200.
@@ -297,19 +297,19 @@ async def test_input_blog_address_output_response_validation(monkeypatch, capsys
     valid_url = f'https://{valid}.blogspot.com/'
     id = '12345'
 
-    def mock_input_func():
+    def mock_input_func() -> str:
         return next(user_inputs)
 
-    async def mock_validate_blog_name(name):
+    async def mock_validate_blog_name(name:str) -> bool:
         return True
 
-    async def mock_check_response_code(name, session):
+    async def mock_check_response_code(name: str, session: aiohttp.ClientSession) -> bool:
         return name == valid
 
-    async def mock_is_blogger(url, session):
+    async def mock_is_blogger(url: str, session: aiohttp.ClientSession) -> bool:
         return True
 
-    async def mock_get_blog_id(url, session):
+    async def mock_get_blog_id(url: str, session: aiohttp.ClientSession) -> str | bool:
         return id
 
     async with aiohttp.ClientSession() as session:
@@ -326,12 +326,102 @@ async def test_input_blog_address_output_response_validation(monkeypatch, capsys
 
 
 @pytest.mark.asyncio
-async def test_validate_blog_name():
+async def test_input_blog_address_output_is_blogger(monkeypatch: pytest.MonkeyPatch,
+                                                             capsys: pytest.fixture) -> None:
+    """
+    Test designed to check for validity of messages presented to the user, when response code
+    is else than 200.
+    :param monkeypatch: Pytest's built-in fixture
+    :param capsys: Pytest's built-in fixture
+    :return: None
+    """
+    valid = 'learningpythonway'
+    invalid = 'http://foobarbazbazsuperduper.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid])
+    valid_url = f'https://{valid}.blogspot.com/'
+    id = '12345'
+
+    def mock_input_func() -> str:
+        return next(user_inputs)
+
+    async def mock_validate_blog_name(name:str) -> bool:
+        return True
+
+    async def mock_check_response_code(name: str, session: aiohttp.ClientSession) -> bool:
+        return True
+
+    async def mock_is_blogger(url: str, session: aiohttp.ClientSession) -> bool:
+        return False
+
+    async def mock_get_blog_id(url: str, session: aiohttp.ClientSession) -> str | bool:
+        return id
+
+    async with aiohttp.ClientSession() as session:
+        monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+        monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+        monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+        monkeypatch.setattr('utils.request_handling.is_blogger', mock_is_blogger)
+        monkeypatch.setattr('utils.request_handling.get_blog_id', mock_get_blog_id)
+
+        assert await req.input_blog_address(session)
+        captured = capsys.readouterr()
+        assert 'Requested blog seems not to have an ID, try one more time.' not in captured.out
+        assert ('Requested blog seems not to be on blogger platform, try one more time.'
+                in captured.out)
+
+
+@pytest.mark.asyncio
+async def test_input_blog_address_output_get_blog_id(monkeypatch: pytest.MonkeyPatch,
+                                                             capsys: pytest.fixture) -> None:
+    """
+    Test designed to check for validity of messages presented to the user, when response code
+    is else than 200.
+    :param monkeypatch: Pytest's built-in fixture
+    :param capsys: Pytest's built-in fixture
+    :return: None
+    """
+    valid = 'learningpythonway'
+    invalid = 'http://foobarbazbazsuperduper.blogspot.com'
+    user_inputs = iter([invalid, invalid, invalid])
+    valid_url = f'https://{invalid}.blogspot.com/'
+    id = '12345'
+
+    def mock_input_func() -> str:
+        return next(user_inputs)
+
+    async def mock_validate_blog_name(name:str) -> bool:
+        return True
+
+    async def mock_check_response_code(name: str, session: aiohttp.ClientSession) -> bool:
+        return True
+
+    async def mock_is_blogger(url: str, session: aiohttp.ClientSession) -> bool:
+        return True
+
+    async def mock_get_blog_id(url: str, session: aiohttp.ClientSession) -> str | bool:
+        return False
+
+    async with aiohttp.ClientSession() as session:
+        monkeypatch.setattr('utils.request_handling.input_func', mock_input_func)
+        monkeypatch.setattr('utils.request_handling.validate_blog_name', mock_validate_blog_name)
+        monkeypatch.setattr('utils.request_handling.check_response_code', mock_check_response_code)
+        monkeypatch.setattr('utils.request_handling.is_blogger', mock_is_blogger)
+        monkeypatch.setattr('utils.request_handling.get_blog_id', mock_get_blog_id)
+
+        with pytest.raises(AssertionError):
+            assert await req.input_blog_address(session)
+        captured = capsys.readouterr()
+        # assert 'Requested blog seems to not respond, try one more time.' not in captured.out
+        assert 'Requested blog seems not to have an ID, try one more time.' in captured.out
+
+
+@pytest.mark.asyncio
+async def test_validate_blog_name() -> None:
     names = iter(['https://blog', 'blog.blogspot.com',
                   'http://bloggg', 'blog.com/',
                   'blog/', 'blog'])
 
-    def provide_names():
+    def provide_names() -> str:
         return next(names)
 
     assert await req.validate_blog_name(provide_names()) is False, 'Should be False but is True'
@@ -344,11 +434,15 @@ async def test_validate_blog_name():
 
 # TODO Change the way of testing, to one using pytest async_aiohttp. Use tutorial on package docs
 #  by setting a web app, and serwer responses responses.
-def test_response_code(requests_mock):
+
+@pytest.mark.asyncio
+async def test_response_code(aiohttp_client, capsys) -> None:
     """
     Test for intercepting requests and mocking the responses.
     Asserts function behaviour for different response codes.
-    :param requests_mock: requests_mock fixture, for intercepting requests send to HTTP servers.
+    :param aiohttp_client: The aiohttp_client fixture available from pytest-aiohttp plugin,
+    which allows the creation of a client to make requests to test the app.
+    :param capsys: Pytest's built-in fixture
     :return: None
     """
     valid_response = 'foobazbar'
@@ -356,20 +450,106 @@ def test_response_code(requests_mock):
     totally_wrong_url = 'some_verry_bad_url.blogspot.pl'
     blogs = iter([valid_response, invalid_response, totally_wrong_url])
 
-    def provide_data():
+    def provide_data() -> tuple[str, str]:
         name_ = next(blogs)
         address = f'https://{name_}.blogspot.com/'
         return name_, address
 
-    name, url = provide_data()
-    requests_mock.get(url, status_code=200)
-    assert req.check_response_code(name) is True
-    name, url = provide_data()
-    requests_mock.get(url, status_code=404)
-    assert req.check_response_code(name) is False
-    name, url = provide_data()
-    requests_mock.get(url, exc=requests.ConnectionError)
-    assert req.check_response_code(name) is False
+    async def server(request):
+        if request.path == f'/https://{valid_response}.blogspot.com/':
+            return web.Response(text='Valid blog address. Valid response', status=200)
+        elif request.path == f'/https://{invalid_response}.blogspot.com/':
+            return web.Response(text='Blog is private, does not exist or something.', status=400)
+        else:
+            return web.Response(text='Invalid address', status=404)
+
+    app = web.Application()
+    app.router.add_get(f'/https://{valid_response}.blogspot.com/', server)
+    app.router.add_get(f'/https://{invalid_response}.blogspot.com/', server)
+    app.router.add_get('/not-responding', server)
+    client = await aiohttp_client(app)
+
+    async with aiohttp.ClientSession() as session:
+        name, url = provide_data()
+        resp = await client.get(f'/{url}')
+        assert resp.status == 200
+        assert await req.check_response_code(name, session) is True
+
+        name, url = provide_data()
+        resp = await client.get(f'/{url}')
+        assert resp.status == 400
+        assert await req.check_response_code(name, session) is False
+
+        name, url = provide_data()
+        resp = await client.get(f'/{url}')
+        assert resp.status == 404
+        assert await req.check_response_code(name, session) is False
+        captured = capsys.readouterr()
+        assert 'Connection Error' in captured.out
+        assert 'Connection Invalid' in captured.out
+        assert 'Connection Valid' in captured.out
+
+        resp = await client.get('/not-responding')
+        assert resp.status == 404
+
+
+# @pytest.mark.asyncio
+# async def test_response_code_error(aiohttp_client, monkeypatch: pytest.MonkeyPatch) -> None:
+#     """
+#
+#     :param aiohttp_client:
+#     :param monkeypatch:
+#     :return:
+#     """
+#
+#     valid_response = 'foobazbar'
+#     invalid_response = 'foobazbaromg'
+#     totally_wrong_url = 'some_verry_bad_url.blogspot.pl'
+#     blogs = iter([totally_wrong_url, valid_response, invalid_response])
+#
+#     def provide_data() -> tuple[str, str]:
+#         name_ = next(blogs)
+#         address = f'https://{name_}.blogspot.com/'
+#         return name_, address
+#
+#     class MockClientSession(aiohttp.ClientSession):
+#         async def request(self, *args, **kwargs):
+#             url_ = args[0]
+#             if url_ == f'https://{totally_wrong_url}.blogspot.pl/':
+#                 raise aiohttp.InvalidURL
+#
+#     async def server(request):
+#         if request.path == f'/https://{valid_response}.blogspot.com/':
+#             return web.Response(text='Valid blog address. Valid response', status=200)
+#         elif request.path == f'/https://{invalid_response}.blogspot.com/':
+#             return web.Response(text='Blog is private, does not exist or something.', status=400)
+#         else:
+#             return web.Response(text='Invalid address', status=404)
+#
+#     app = web.Application()
+#     app.router.add_get(f'/https://{valid_response}.blogspot.com/', server)
+#     app.router.add_get(f'/https://{invalid_response}.blogspot.com/', server)
+#     app.router.add_get('/not-responding', server)
+#     client = await aiohttp_client(app)
+#
+#     monkeypatch.setattr(aiohttp, 'ClientSession', MockClientSession)
+#
+#     async with aiohttp.ClientSession() as session:
+#         name, url = provide_data()
+#         with pytest.raises(aiohttp.ClientConnectorError):
+#             await req.check_response_code(name, session)
+#         assert await req.check_response_code(name, session) is False
+
+    # requests_mock.get(url, status_code=200)
+    # assert req.check_response_code(name) is True
+    # name, url = provide_data()
+    # requests_mock.get(url, status_code=404)
+    # assert req.check_response_
+    # code(name) is False
+    # name, url = provide_data()
+    # requests_mock.get(url, exc=requests.ConnectionError)
+    # assert req.check_response_code(name) is False
+
 
 
 # def test_is_blogger(requests_mock):
