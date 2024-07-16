667fde6d89f709be5598d394e158b3ebb016fa56
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-16 22:26:34 +0200

    cleaned up main function.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- main.py
+++ main.py
@@ -8,35 +8,14 @@ import utils.request_handling as req
 import time
 
 
-def main():
-    start = time.time()
-    with open(config.KEY, encoding='utf-8') as file:
-        api_key = json.load(file)['Authorization']
-
-    blog_id = req.input_blog_address()
-    # blog_id = req.get_blog_id(address)
-    url = req.return_request_url('by_id', blog_id=blog_id, auth=api_key,
-                                 base_req_body=config.API_BASE_REQ)
-    with requests.Session() as s:
-        r = s.get(url)
-        print(r.text)
-    end = time.time()
-    print(f'Elapsed: {end - start:.2f}')
-    # blog = req.get_blog_id('smakolykibereniki.blogspot.com')
-    # print(blog)
-
-
-async def create_session():
-    return aiohttp.ClientSession()
-
-
 async def main2():
     start = time.time()
-    with open(config.KEY, encoding='utf-8') as file:
+    with open(config.KEY, encoding='UTF-8') as file:
         api_key = json.load(file)['Authorization']
 
     async with await req.ConnectionPool.get_session() as session:
         blog_id = await req.input_blog_address(session)
+        print('blog id: ', blog_id)
 
 
 
@@ -44,11 +23,18 @@ async def main2():
     print(f'Elapsed: {end - start:.2f}')
 
 
-# TODO create tests for new functions: input_func, input_blog_address, validate_blog_name,
-#  check_response_code, is_blogger, get_blog_id
+# TODO check how to catch errors from aiohttp or asyncio,
+#  while other libraries return exceptions
 
 
 if __name__ == '__main__':
-    asyncio.run(main2())
+    asyncio.run(main2(), debug=True)
 
 
+# async def main3():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get('https://googlevideo87263e987?.gog.pl?)__+)L!@*&(') as resp:
+#             print(resp.status)
+#             print(await resp.text())
+#
+# asyncio.run(main3())
