7bfe55c8e388e32a935b84cf66cb0eda90a87910
MrSz-84 <pixelmotionhd@gmail.com>
2024-07-16 22:28:10 +0200

    More cleaning and some refactoring.
    Signed-off-by: MrSz-84 <pixelmotionhd@gmail.com>

--- main.py
+++ main.py
@@ -8,33 +8,19 @@ import utils.request_handling as req
 import time
 
 
-async def main2():
+async def main():
     start = time.time()
-    with open(config.KEY, encoding='UTF-8') as file:
+    with open(config.KEY, encoding='utf-8') as file:
         api_key = json.load(file)['Authorization']
 
     async with await req.ConnectionPool.get_session() as session:
         blog_id = await req.input_blog_address(session)
         print('blog id: ', blog_id)
 
-
-
     end = time.time()
     print(f'Elapsed: {end - start:.2f}')
 
 
-# TODO check how to catch errors from aiohttp or asyncio,
-#  while other libraries return exceptions
-
-
 if __name__ == '__main__':
-    asyncio.run(main2(), debug=True)
+    asyncio.run(main(), debug=True)
 
-
-# async def main3():
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get('https://googlevideo87263e987?.gog.pl?)__+)L!@*&(') as resp:
-#             print(resp.status)
-#             print(await resp.text())
-#
-# asyncio.run(main3())
